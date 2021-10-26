0b98e557ee3ac423e349d80d1c655ac5fa0fdd59
Munesh Sutar <85051705+muneshsutar@users.noreply.github.com>
2021-10-26 12:07:24 +0530

    Android Studio

---
+++ app/src/main/java/com/munesh/readiobutton/MainActivity.java
@@ -0,0 +1,38 @@
+package com.munesh.readiobutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        radioGroup = findViewById(R.id.radioGender);
+        button = findViewById(R.id.btnDisplay);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedID = radioGroup.getCheckedRadioButtonId();
+                radioButton = findViewById(selectedID);
+                Toast.makeText(MainActivity.this, radioButton.getText(),Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
+
+
+
+
